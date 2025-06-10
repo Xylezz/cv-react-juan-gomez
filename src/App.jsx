ff55e098a42b6e719f04144c3d152f832d456830
Xylezz <juanda200713@gmail.com>
2025-06-10 20:30:33 +0000

    Actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,3 @@
-// ToggleHabilidades.jsx
-import React from 'react';
-
-const ToggleHabilidades = ({ visible, toggleVisibilidad }) => {
-  return (
-    <div>
-      <button onClick={toggleVisibilidad}>
-        {visible ? 'Ocultar Habilidades' : 'Mostrar Habilidades'}
-      </button>
-    </div>
-  );
-};
-
-export default ToggleHabilidades;
 // src/App.jsx
 import React, { useState } from "react";
 import {
